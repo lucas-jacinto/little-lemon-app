26a6c23416f7b962f74c5182b02c85ea9abe66fd
lucas jacinto dos santos <101364011+lucas-jacinto@users.noreply.github.com>
2023-02-11 13:02:37 +0000

    changes in the structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css'
+import Nav from '../src/components/Nav'
+import Header from '../src/components/Header'
+import Main from '../src/components/Main'
+import Footer from '../src/components/Footer'
 
 function App() {
-  return <div className="App">Homepage</div>
+  return <>
+    <Nav></Nav>
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
+  </>
 }
 
 export default App
