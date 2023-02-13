4819eaa285bf9acfa6420fb9f8bc4e45f1f6454d
lucas jacinto dos santos <101364011+lucas-jacinto@users.noreply.github.com>
2023-02-13 19:13:48 +0000

    inserting css

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './nav.css'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='top'> 
+      <div className='logo'>
+
+      </div>
+      <div className='icons'>
+        <h2>Menu</h2>
+        <h2>Services</h2>
+        <h2>Reserve a Table</h2>
+        <h2>About Us</h2>
+      </div>
+    </div>
   )
 }
 
