b1b41ec6bf2a1373b29d6ef1d8bab8223bd6ba4c
Lucas Jacinto <lucasjdseng@gmail.com>
2023-02-21 11:11:52 -0300

    ading hero section1

--- src/components/HeroSection/Header.jsx
+++ src/components/HeroSection/Header.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './header.css'
+import logo2 from '../../assets/restauranfood.jpg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='hero-header'>
+      <div className='hero'>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <p>We are a family owned Mediterranean restaurant, focused on traditionl recipes served with a modern twist.</p>
+        <button className='btn-header'>Reserve a Table
+        </button>
+      </div>
+      <div className='img-header'>
+        <img src={logo2} alt="" />
+      </div>
+    </header>
   )
 }
 
