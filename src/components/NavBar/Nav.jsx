babec5dde478c943941d7c9e6151750766d0316a
Lucas Jacinto <lucasjdseng@gmail.com>
2023-02-21 17:45:40 -0300

    creating specials section

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -5,6 +5,7 @@ import logo from '../../assets/Logo.svg'
 const Nav = () => {
   return (
     <nav>
+
       <div className='top'>
 
         <div className='logo'>
@@ -12,16 +13,12 @@ const Nav = () => {
         </div>
 
         <div className='icons'>
-          <ul>
-            <li className='icons-nav'>
-              <a href='#'>Home</a>
+              <a href='#home'>Home</a>
               <a href='#about'>About</a>
               <a href='#menu'>Menu</a>
               <a href='#reserve'>Reservtions</a>
               <a href='#order'>Order Online</a>
               <a href='#login'>Login</a>
-            </li>
-          </ul>
         </div>
       </div>
     </nav>
--- src/components/Specials/Specials.jsx
+++ src/components/Specials/Specials.jsx
@@ -4,9 +4,26 @@ import './specials.css'
 
 const Specials = () => {
   return (
-    <main>
-    Specials
-    </main>
+    <section>
+      <div className='main-specials'>
+        <h1 className='h1-main'>This weeks specials!</h1>
+        <div className='btn-special'>
+          <button href='#' className='btn-specials'>Online Menu</button>
+        </div>
+      </div>
+      <div className='card-specials'>
+        <div className='cards'>
+
+        </div>
+        <div className='cards'>
+
+        </div>
+        <div className='cards'>
+
+        </div>
+      </div>
+    </section>
+
   )
 }
 
