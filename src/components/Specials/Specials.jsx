e73a59a2d2722a6885674fb9f11fdde15b49357b
Lucas Jacinto <lucasjdseng@gmail.com>
2023-02-21 18:45:39 -0300

    changes2

--- src/components/Specials/Specials.jsx
+++ src/components/Specials/Specials.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-
+import salad from '../../assets/greek salad.jpg'
+import bruchetta from '../../assets/bruchetta.svg'
+import lemon from '../../assets/lemon dessert.jpg'
+import delivery from '../../assets/basket .svg'
 import './specials.css'
 
 const Specials = () => {
@@ -13,13 +16,20 @@ const Specials = () => {
       </div>
       <div className='card-specials'>
         <div className='cards'>
+          <img src={salad} alt="greek salad image" />
+          <h4>Greek salad </h4> <span>$12.99</span>
 
+          <p>The famous greek salad of crispy lettuce, pepers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.</p>
+
+          <a href="#delivery">Order delivery <img src={delivery} alt="" /></a>
         </div>
-        <div className='cards'>
-
+        <div className='cards bru'>
+          <img src={bruchetta} alt="greek salad image" />
+          <h4>Greek salad <span>$12.99</span> </h4>
         </div>
-        <div className='cards'>
-
+        <div className='cards lemo'>
+          <img src={lemon} alt="greek salad image" />
+          <h4>Greek salad </h4> <span>$12.99</span>
         </div>
       </div>
     </section>
